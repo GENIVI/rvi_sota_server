f677613ecfb00f201512925f8f054e521cf27779
Vladimir Koshelev <vladimir@advancedtelematic.com>
2015-07-21 17:33:16 +0200

    Docker packaging for core and resolver

---
+++ project/Packaging.scala
@@ -0,0 +1,16 @@
+object Packaging {
+  import com.typesafe.sbt.packager.archetypes.JavaAppPackaging
+  import com.typesafe.sbt.packager.docker.DockerPlugin
+  import com.typesafe.sbt.packager.Keys._
+  import sbt.Keys._
+  import DockerPlugin.autoImport.Docker
+  import com.typesafe.sbt.SbtGit.git
+
+  lazy val settings = Seq(
+    packageName in Docker := "sota-" + packageName.value,
+    version in Docker := git.gitDescribedVersion.value.get,
+    dockerUpdateLatest in Docker := true
+  )
+
+  val plugins = Seq(DockerPlugin, JavaAppPackaging)
+}
