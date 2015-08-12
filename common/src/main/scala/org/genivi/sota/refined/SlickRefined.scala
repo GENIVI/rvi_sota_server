405135cfd98e7ec0c752d207eddf6b03e8b31e82
Stevan Andjelkovic <stevan@advancedtelematic.com>
2015-08-12 15:55:36 +0200

    Remove old unused shapeless.tag validation stuff.

--- common/src/main/scala/org/genivi/sota/refined/SlickRefined.scala
+++ common/src/main/scala/org/genivi/sota/refined/SlickRefined.scala
@@ -6,7 +6,6 @@ package org.genivi.sota.refined
 
 import eu.timepit.refined.internal.Wrapper
 import eu.timepit.refined.{Predicate, Refined}
-import shapeless.tag._
 
 trait SlickRefined {
   import slick.driver.MySQLDriver.api._
@@ -22,9 +21,6 @@ trait SlickRefined {
     }
 
   }
-
-  implicit def refinedTMappedType[T, P]( implicit delegate: ColumnType[T], predicate: Predicate[P, T] ) : ColumnType[T @@ P] =
-    MappedColumnType.base[T @@ P, T]( identity, x => Wrapper.tagWrapper.wrap[T, P](x) )
 
   implicit def refinedMappedType[T, P, F[_, _]]
       (implicit delegate: ColumnType[T],
--- common/src/main/scala/org/genivi/sota/refined/SprayJsonRefined.scala
+++ common/src/main/scala/org/genivi/sota/refined/SprayJsonRefined.scala
@@ -4,29 +4,16 @@
  */
 package org.genivi.sota.refined
 
-trait SprayJsonRefined {
-  import eu.timepit.refined.{Predicate, _}
-  import shapeless.tag.@@
-  import spray.json._
+import eu.timepit.refined.{Predicate, Refined, refineV}
+import spray.json._
+import scala.util.control.NoStackTrace
 
-  import scala.util.control.NoStackTrace
-  import scala.util.{Left, Right}
+
+trait SprayJsonRefined {
 
   case class RefinmentError[T]( o: T, msg: String) extends NoStackTrace
 
-  implicit def refinedTJsonFormat[T, P] (implicit delegate: JsonFormat[T], predicate: Predicate[P, T]) : JsonFormat[T @@ P] =
-    new JsonFormat[T @@ P] {
-      override def write( o: T @@ P ) : JsValue = delegate.write( o )
-
-      override def read(value : JsValue) : T @@ P = {
-        refineT[P]( delegate.read(value) ) match {
-          case Right(x) => x
-          case Left(e) => deserializationError(e, RefinmentError(value, e))
-        }
-      }
-    }
-
-  implicit def refinedJsonFormat[T, P](implicit delegate: JsonFormat[T], predicate: Predicate[P, T]) : JsonFormat[Refined[T, P]] =
+  implicit def refinedJsonFormat[T, P](implicit delegate: JsonFormat[T], p: Predicate[P, T]): JsonFormat[Refined[T, P]] =
     new JsonFormat[Refined[T, P]] {
       override def write(o: Refined[T, P]): JsValue = delegate.write(o.get)
 
@@ -34,7 +21,7 @@ trait SprayJsonRefined {
         val value = delegate.read(json)
         refineV[P]( value ) match {
           case Right(x) => x
-          case Left(e) => deserializationError(e, RefinmentError(value, e))
+          case Left(e)  => deserializationError(e, RefinmentError(value, e))
         }
       }
     }
