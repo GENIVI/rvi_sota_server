76e25855c13d68edd7967ad9b9b42ebea1942639
Stevan Andjelkovic <stevan@advancedtelematic.com>
2015-08-20 14:35:31 +0200

    Refactor tests.

--- external-resolver/src/test/scala/org/genivi/sota/resolver/test/ResolveResourceSpec.scala
+++ external-resolver/src/test/scala/org/genivi/sota/resolver/test/ResolveResourceSpec.scala
@@ -1,16 +1,5 @@
 package org.genivi.sota.resolver.test
 
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.model.StatusCodes
-import akka.http.scaladsl.server.Route
-import eu.timepit.refined.Refined
-import org.genivi.sota.refined.SprayJsonRefined._
-import org.genivi.sota.resolver.db.Resolve.makeFakeDependencyMap
-import org.genivi.sota.resolver.types.Package
-import org.genivi.sota.resolver.types.Package.Metadata
-import org.genivi.sota.resolver.types.{Vehicle, Filter, PackageFilter}
-import spray.json.DefaultJsonProtocol._
-
 
 class ResolveResourceWordSpec extends ResourceWordSpec {
 
@@ -19,64 +8,35 @@ class ResolveResourceWordSpec extends ResourceWordSpec {
     "give back a list of vehicles onto which the supplied package should be installed" in {
 
       // Add some vehicles.
-      Put(VehiclesUri("00RESOLVEVIN12345")) ~> route ~> check {
-        status shouldBe StatusCodes.NoContent
-      }
-      Put(VehiclesUri("01RESOLVEVIN12345")) ~> route ~> check {
-        status shouldBe StatusCodes.NoContent
-      }
-      Put(VehiclesUri("10RESOLVEVIN12345")) ~> route ~> check {
-        status shouldBe StatusCodes.NoContent
-      }
-      Put(VehiclesUri("11RESOLVEVIN12345")) ~> route ~> check {
-        status shouldBe StatusCodes.NoContent
-      }
+      val vins = List(
+        "00RESOLVEVIN12345",
+        "01RESOLVEVIN12345",
+        "10RESOLVEVIN12345",
+        "11RESOLVEVIN12345")
+
+      vins map addVehicleOK
 
       // Add a package.
-      addPackage("resolve pkg", "0.0.1", None, None) ~> route ~> check {
-        status shouldBe StatusCodes.OK
-      }
+      addPackageOK("resolve pkg", "0.0.1", None, None)
 
       // Add a trival filter that lets all vins through.
+      addFilterOK("truefilter", "TRUE")
+      addPackageFilterOK("resolve pkg", "0.0.1", "truefilter")
 
-      def addFilter(name: String, expr: String): Unit =
-        Post(FiltersUri, Filter(Refined(name), Refined(expr))) ~> route ~> check {
-          status shouldBe StatusCodes.OK
-        }
-
-      addFilter("truefilter", "TRUE")
-
-      // Associate filters to package.
-
-      def assocFilterToPackage(pname: String, pversion: String, fname: String): Unit =
-        Post(PackageFiltersUri, PackageFilter(Refined(pname), Refined(pversion), Refined(fname))) ~> route ~> check {
-          status shouldBe StatusCodes.OK
-        }
-
-      assocFilterToPackage("resolve pkg", "0.0.1", "truefilter")
-
-      def resolve(pname: String, pversion: String, vins: Seq[String]): Unit =
-        Get(ResolveUri(pname, pversion)) ~> route ~> check {
-          status shouldBe StatusCodes.OK
-          responseAs[Map[Vehicle.Vin, List[Package.Id]]] shouldBe
-            makeFakeDependencyMap(Refined(pname), Refined(pversion), vins.map(s => Vehicle(Refined(s))))
-        }
-
-      resolve("resolve pkg", "0.0.1",
-        List("00RESOLVEVIN12345", "01RESOLVEVIN12345", "10RESOLVEVIN12345", "11RESOLVEVIN12345"))
+      resolveOK("resolve pkg", "0.0.1", vins)
 
       // Add another filter.
-      addFilter("0xfilter", s"""vin_matches "^00.*" OR vin_matches "^01.*"""")
-      assocFilterToPackage("resolve pkg", "0.0.1", "0xfilter")
+      addFilterOK("0xfilter", s"""vin_matches "^00.*" OR vin_matches "^01.*"""")
+      addPackageFilterOK("resolve pkg", "0.0.1", "0xfilter")
 
-      resolve("resolve pkg", "0.0.1",
+      resolveOK("resolve pkg", "0.0.1",
         List("00RESOLVEVIN12345", "01RESOLVEVIN12345"))
 
       // Add trivially false filter.
-      addFilter("falsefilter", "FALSE")
-      assocFilterToPackage("resolve pkg", "0.0.1", "falsefilter")
+      addFilterOK("falsefilter", "FALSE")
+      addPackageFilterOK("resolve pkg", "0.0.1", "falsefilter")
 
-      resolve("resolve pkg", "0.0.1", List())
+      resolveOK("resolve pkg", "0.0.1", List())
 
     }
   }
--- external-resolver/src/test/scala/org/genivi/sota/resolver/test/ResourceSpec.scala
+++ external-resolver/src/test/scala/org/genivi/sota/resolver/test/ResourceSpec.scala
@@ -4,45 +4,28 @@
  */
 package org.genivi.sota.resolver.test
 
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.model.{Uri, HttpRequest}
-import akka.http.scaladsl.model.Uri.Path
+import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import org.flywaydb.core.Flyway
-import org.genivi.sota.resolver.db.{Vehicles, Packages}
-import org.genivi.sota.resolver.types.{Vehicle$, Package}
+import org.genivi.sota.resolver.Routing
 import org.scalatest.prop.PropertyChecks
 import org.scalatest.{BeforeAndAfterAll, Suite, WordSpec, PropSpec, Matchers}
 import slick.jdbc.JdbcBackend.Database
 
 
 trait ResourceSpec extends Matchers
+    with VehicleRequests
+    with PackageRequests
+    with FilterRequests
+    with PackageFilterRequests
+    with ResolveRequests
     with ScalatestRouteTest
     with BeforeAndAfterAll { self: Suite =>
-
-  // URIs
-  val BasePath = Path("/api") / "v1"
-  val VehiclesUri           = (vin: String) => resourceUri("vehicles", vin)
-  val ResolveUri            = (name: String, version: String) => resourceUri("resolve", name, version)
-  val FiltersUri            = resourceUri("filters")
-  val ValidateUri           = (s: String) => resourceUri("validate", s)
-  val PackageFiltersUri     = resourceUri("packageFilters")
-
-  def PackageFiltersListUri(ss: String*) =
-    resourceUri(("packageFilters" +: ss): _*)
-
-  def resourceUri(pathSuffixes: String*): Uri =
-    Uri.Empty.withPath(pathSuffixes.foldLeft(BasePath)(_/_))
-
-  // Operations on the resources.
-  def addPackage
-    (name: String, version: String, desc: Option[String], vendor: Option[String])
-      : HttpRequest
-  = Put(resourceUri("packages", name, version), Package.Metadata(desc, vendor))
 
   // Database
   val name = "test-database"
   val db = Database.forConfig(name)
+
 
   override def beforeAll() = {
     val dbConfig = system.settings.config.getConfig(name)
@@ -63,7 +46,8 @@ trait ResourceSpec extends Matchers
   }
 
   // Route
-  lazy val route = new org.genivi.sota.resolver.Routing(db).route
+  lazy implicit val route: Route = new org.genivi.sota.resolver.Routing(db).route
+
 }
 
 trait ResourceWordSpec extends WordSpec with ResourceSpec
