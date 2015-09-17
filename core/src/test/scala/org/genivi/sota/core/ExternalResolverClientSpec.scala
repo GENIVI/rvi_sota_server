bb2b8791e8e19fb7de379b49c0382a049a93e5fc
Stevan Andjelkovic <stevan@advancedtelematic.com>
2015-09-17 16:42:30 +0200

    Add some more tests for the resolve route.

--- core/src/test/scala/org/genivi/sota/core/ExternalResolverClientSpec.scala
+++ core/src/test/scala/org/genivi/sota/core/ExternalResolverClientSpec.scala
@@ -3,8 +3,11 @@ package org.genivi.sota.core
 import akka.actor.ActorSystem
 import akka.http.scaladsl.model.{HttpResponse, StatusCodes, Uri}
 import akka.stream.ActorMaterializer
+import cats.data.Xor
+import eu.timepit.refined.Refined
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{BeforeAndAfterAll, Matchers, PropSpec}
+import org.genivi.sota.core.data.{Vehicle, Package}
 
 import scala.concurrent.Future
 
@@ -28,8 +31,22 @@ class ExternalResolverClientSpec extends PropSpec with Matchers with BeforeAndAf
     ScalaFutures.whenReady( client.handlePutResponse( Future.successful( HttpResponse(StatusCodes.BadRequest) ) ).failed ) { e =>
       e shouldBe a [ExternalResolverRequestFailed]
     }
-
   }
+
+  val s: String = s"""[[{"get":"VINBEAGLEBOARD000"},[{"version":{"get":"23.5.2"},"name":{"get":"rust"}}]]]"""
+
+  val m: Map[Vehicle.IdentificationNumber, Set[Package.Id]] =
+    Map(Refined("VINBEAGLEBOARD000") -> Set(Package.Id(Refined("rust"), Refined("23.5.2"))))
+
+  property("parse the external resolver's response") {
+
+    import io.circe.generic.auto._
+    import io.circe.jawn._
+
+    decode[Map[Vehicle.IdentificationNumber, Set[Package.Id]]](s) shouldBe Xor.Right(m)
+  }
+
+  val s2: String = s"""[[{"get":"VINBEAGLEBOARD000"},[{"version":"23.5.2","name":"rust"}]]]"""
 
   override def afterAll(): Unit = {
     system.shutdown()
