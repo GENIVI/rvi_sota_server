4b75ab67912d27c92d918a0e4c55654696113493
Calum McCall <calum@advancedtelematic.com>
2015-10-07 15:20:42 +0200

    Add summary counts for failed/completed VINs
    This commit also introduces buttons to list VINs which have failed/
    completed a given update.
    
    SOTA-76

--- web-server/app/assets/javascripts/components/updates/show-status.jsx
+++ web-server/app/assets/javascripts/components/updates/show-status.jsx
@@ -17,9 +17,103 @@ define(function(require) {
       });
       this.props.UpdateStatus.addWatch("poll-update-status", _.bind(this.forceUpdate, this, null));
     },
+    failedVINsTable: function() {
+      var failedVINRows = _.map(this.props.UpdateStatus.deref(), function(value) {
+        if(Array.isArray(value)) {
+          if(value[2] === "Failed") {
+            return (
+              <tr>
+                <td>
+                  {value[1]}
+                </td>
+              </tr>
+            )
+          }
+        }
+      });
+      return (
+        <div>
+          <div className="row">
+            <div className="col-md-12">
+              <h2>
+                Failed VINs
+              </h2>
+            </div>
+          </div>
+          <br/>
+          <div className="row">
+            <div className="col-xs-12">
+              <table className="table table-striped table-bordered">
+                <tbody>
+                  { failedVINRows }
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      );
+    },
+    completedVINsTable: function() {
+      var completedVINRows = _.map(this.props.UpdateStatus.deref(), function(value) {
+        if(Array.isArray(value)) {
+          if(value[2] === "Completed") {
+            return (
+              <tr>
+                <td>
+                  {value[1]}
+                </td>
+              </tr>
+            )
+          }
+        }
+      });
+      return (
+        <div>
+          <div className="row">
+            <div className="col-md-12">
+              <h2>
+                Completed VINs
+              </h2>
+            </div>
+          </div>
+          <br/>
+          <div className="row">
+            <div className="col-xs-12">
+              <table className="table table-striped table-bordered">
+                <tbody>
+                  { completedVINRows }
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      );
+    },
+    toggleFailedVINs: function() {
+      this.setState({showFailedVINs: !this.state.showFailedVINs});
+    },
+    toggleCompletedVINs: function() {
+      this.setState({showCompletedVINs: !this.state.showCompletedVINs});
+    },
+    getInitialState: function() {
+      return {showFailedVINs: false,
+              showCompletedVINs: false
+      };
+    },
     render: function() {
+      var completedVINs = 0;
+      var pendingVINs = 0;
+      var failedVINs = 0;
+
       var rows = _.map(this.props.UpdateStatus.deref(), function(value) {
         if(Array.isArray(value)) {
+          if(value[2] === "Pending") {
+            pendingVINs++;
+          } else if (value[2] === "Completed") {
+            completedVINs++;
+          } else if(value[2] === "Failed") {
+            failedVINs++;
+          }
           return (
             <tr>
               <td>
@@ -43,7 +137,43 @@ define(function(require) {
           </div>
           <br/>
           <div className="row">
+            <div className="col-xs-12">
+              <table className="table table-striped table-bordered">
+              <tbody>
+                <tr>
+                  <td>Pending:</td>
+                  <td>{pendingVINs}</td>
+                </tr>
+                <tr>
+                  <td>Completed:</td>
+                  <td>{completedVINs}</td>
+                </tr>
+                <tr>
+                  <td>Failed:</td>
+                  <td>{failedVINs}</td>
+                </tr>
+              </tbody>
+              </table>
+            </div>
+          </div>
+          <button className="btn btn-primary pull-right" onClick={this.toggleFailedVINs}>
+            { this.state.showFailedVINs ? "HIDE" : "Show failed VINs" }
+          </button>
+          <button className="btn btn-primary pull-right" onClick={this.toggleCompletedVINs}>
+            { this.state.showCompletedVINs ? "HIDE" : "Show completed VINs" }
+          </button>
+          { this.state.showFailedVINs ? this.failedVINsTable() : null }
+          { this.state.showCompletedVINs ? this.completedVINsTable() : null }
+          <div className="row">
             <div className="col-md-12">
+              <h2>
+                All VINs
+              </h2>
+            </div>
+          </div>
+          <br/>
+          <div className="row">
+            <div className="col-xs-12">
               <table className="table table-striped table-bordered">
                 <tbody>
                   { rows }
