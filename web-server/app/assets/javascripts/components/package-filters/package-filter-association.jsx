01ada965f28e3f3bc747a06a01c7d79c4027aa4b
Calum McCall <calum@advancedtelematic.com>
2015-10-28 16:18:27 +0100

    Display package id in filter screen for clarity
    This fixes SOTA-156

--- web-server/app/assets/javascripts/components/package-filters/package-filter-association.jsx
+++ web-server/app/assets/javascripts/components/package-filters/package-filter-association.jsx
@@ -46,7 +46,7 @@ define(function(require) {
     },
 
     listItems: function(model, eventName) {
-      var label = model.id ? model.id.name : model.name;
+      var label = model.id ? model.id.name + '-' + model.id.version : model.name;
       return (
         <PackageFilterListItem Payload={this.payload(model)} eventName={eventName} label={label}/>
       );
