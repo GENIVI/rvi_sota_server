12f1890cdadeff81db2a6e38bb3490c05493b25f
Simão Mata <simao@advancedtelematic.com>
2016-06-08 11:44:22 +0200

    use exec on entrypoints

--- deploy/entrypoint-core.sh
+++ deploy/entrypoint-core.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -z "$DB_ALIVE_URL" ]; then
-	bin/sota-core $@
+	exec bin/sota-core $@
 else
-	./wait-for-it.sh $DB_ALIVE_URL -s -t 120 && bin/sota-core $@
+	./wait-for-it.sh $DB_ALIVE_URL -s -t 120 && exec bin/sota-core $@
 fi
