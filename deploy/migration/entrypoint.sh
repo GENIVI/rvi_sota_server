c56507143cdb1c1e621450561ce6c68b614a141c
Simão Mata <simao@advancedtelematic.com>
2016-06-22 11:08:00 +0200

    refactor migrations container entrypoint

--- deploy/migration/entrypoint.sh
+++ deploy/migration/entrypoint.sh
@@ -9,42 +9,62 @@ function parse_jdbc_url {
     echo $1 | sed -r -e 's/^.+\/\/(.+):([0-9]+).+/\1 \2/'
 }
 
-cd /migrations
 
+MIGRATIONS_WORKDIR=${MIGRATIONS_WORKDIR:-/migrations}
+CLEAN_ALL=${CLEAN_ALL:-false}
 CORE_DB_MIGRATE=${CORE_DB_MIGRATE:-}
 RESOLVER_DB_MIGRATE=${RESOLVER_DB_MIGRATE:-}
 
 MYSQL=$(parse_jdbc_url $CORE_DB_URL)
 
+cd $MIGRATIONS_WORKDIR
+
 ./wait_for_mysql.sh $MYSQL $CORE_DB_USER $CORE_DB_PASS 1 60
 
 git -C rvi_sota_server pull
 
-if [[ "$CORE_DB_MIGRATE" == "true" ]]; then
-    flyway/flyway -url=$CORE_DB_URL \
+
+function core_flyway() {
+        flyway/flyway -url=$CORE_DB_URL \
               -user=$CORE_DB_USER -password=$CORE_DB_PASS \
               -locations=filesystem:./rvi_sota_server/core/src/main/resources/db/migration \
-              migrate
+              $*
+}
+
+function resolver_flyway() {
+        flyway/flyway -url=$RESOLVER_DB_URL \
+              -user=$RESOLVER_DB_USER -password=$RESOLVER_DB_PASS \
+              -locations=filesystem:./rvi_sota_server/external-resolver/src/main/resources/db/migration \
+              $*
+}
+
+
+if [[ "$CLEAN_ALL" == "true" ]]; then
+    read -p "Are you sure you want to clean the database? [YES/no]" prompt
+
+    if [[ "$prompt" == "YES" ]]; then
+        core_flyway clean
+        resolver_flyway clean
+    else
+        echo "clean cancelled"
+    fi
+fi
+
+
+
+if [[ "$CORE_DB_MIGRATE" == "true" ]]; then
+    core_flyway migrate
 else
     echo -e "\e[31;1m\$CORE_DB_MIGRATE false, not migrating\e[0m"
 fi
 
 if [[ "$RESOLVER_DB_MIGRATE" == "true" ]]; then
     echo "$RESOLVER_DB_MIGRATE false, not migrating"
-    flyway/flyway -url=$RESOLVER_DB_URL \
-              -user=$RESOLVER_DB_USER -password=$RESOLVER_DB_PASS \
-              -locations=filesystem:./rvi_sota_server/external-resolver/src/main/resources/db/migration \
-              migrate
+    resolver_flyway migrate
 else
     echo -e "\e[31;1m\$RESOLVER_DB_MIGRATE false, not migrating\e[0m"
 fi
     
-flyway/flyway -url=$CORE_DB_URL \
-              -user=$CORE_DB_USER -password=$CORE_DB_PASS \
-              -locations=filesystem:./rvi_sota_server/core/src/main/resources/db/migration \
-              info
+core_flyway info
 
-flyway/flyway -url=$RESOLVER_DB_URL \
-              -user=$RESOLVER_DB_USER -password=$RESOLVER_DB_PASS \
-              -locations=filesystem:./rvi_sota_server/external-resolver/src/main/resources/db/migration \
-              info
+resolver_flyway info
