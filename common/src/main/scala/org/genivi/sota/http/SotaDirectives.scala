080ec34fbe47484dae4394c85de9ad9069c723bf
Simão Mata <simao@advancedtelematic.com>
2016-06-24 17:08:14 +0200

    add support for extra metrics when logging

--- common/src/main/scala/org/genivi/sota/http/SotaDirectives.scala
+++ common/src/main/scala/org/genivi/sota/http/SotaDirectives.scala
@@ -13,29 +13,40 @@ import akka.http.scaladsl.server.{Directive, Directive0, Directives}
 object SotaDirectives {
   import Directives._
 
+  type MetricsBuilder = (HttpRequest, HttpResponse) => Map[String, String]
+
   def versionHeaders(version: String): Directive0 = {
     val header = RawHeader("x-ats-version", version)
     respondWithHeader(header)
   }
 
-  private def formatResponseLog(service: String, request: HttpRequest,
-                                response: HttpResponse, serviceTime: Long): String = {
-    val metrics = Map(
+  private def defaultMetrics(request: HttpRequest, response: HttpResponse, serviceTime: Long):
+  Map[String, String] = {
+    Map(
       "method" -> request.method.name,
       "path" -> request.uri.path.toString,
       "stime" -> serviceTime.toString,
       "status" -> response.status.intValue.toString
     )
+  }
 
+  private def formatResponseLog(metrics: Map[String, String]): String = {
     metrics.toList.map { case (m, v) => s"$m=$v"}.mkString(" ")
   }
 
-  def logResponseMetrics(service: String, level: LogLevel = Logging.InfoLevel): Directive0 = {
+  def logResponseMetrics(service: String,
+                         extraMetrics: MetricsBuilder = (_, _) => Map.empty,
+                         level: LogLevel = Logging.InfoLevel): Directive0 = {
     extractRequestContext flatMap { ctx =>
       val startAt = System.currentTimeMillis()
       mapResponse { resp =>
         val responseTime = System.currentTimeMillis() - startAt
-        ctx.log.log(level, formatResponseLog(service, ctx.request, resp, responseTime))
+        val allMetrics =
+          defaultMetrics(ctx.request, resp, responseTime) ++
+            extraMetrics(ctx.request, resp) ++ Map("service_name" -> service)
+
+        ctx.log.log(level, formatResponseLog(allMetrics))
+
         resp
       }
     }
