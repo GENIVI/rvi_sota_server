3f5398d44566c1b6b4d2eb93a47614d5371b6820
Miguel Garcia <mgarcia@advancedtelematic.com>
2016-06-30 15:42:58 +0200

    PRO-183 dealing with deprecations in tests

--- web-server/test/ApplicationSpec.scala
+++ web-server/test/ApplicationSpec.scala
@@ -5,20 +5,22 @@
 
 import play.api.test.Helpers._
 import org.scalatestplus.play._
-import play.api.libs.ws.WS
+import play.api.libs.ws.WSClient
 
 /**
  * Test the Application controller
  */
 class ApplicationSpec extends PlaySpec with OneServerPerSuite {
 
+  val wsClient = app.injector.instanceOf[WSClient]
+
   "send 404 on a bad request" in {
-    val response = await(WS.url(s"http://localhost:$port/invalid").get())
+    val response = await(wsClient.url(s"http://localhost:$port/invalid").get())
     response.status mustBe NOT_FOUND
   }
 
   "render the index page" in {
-    val response = await(WS.url(s"http://localhost:$port/").get())
+    val response = await(wsClient.url(s"http://localhost:$port/").get())
     response.status mustBe OK
   }
 
