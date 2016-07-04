13fcfa8ec4d27546f9610318d3ec995412964bca
Jerry Trieu <jerry@advancedtelematic.com>
2016-07-04 14:50:33 +0200

    Use multi files in docker-compose/
    * Rename server-bare.yml to default docker-compose.yml
    * Add core-rvi.yml,client-rvi.yml
    * Use image advancedtelematic/rvi:0.5.0
    * Update docs for docker-compose
    * Remove unused down-all.sh,pull-all.sh

--- deploy/pull-all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -eu
-
-docker-compose -p sota -f docker-compose/server-bare.yml pull
-docker-compose -p sota -f docker-compose/server-rvi.yml  pull
-docker-compose -p sota -f docker-compose/server-rvi-client.yml pull
