028cae0630153d7c8345d430f97db6cc02adf712
Mateusz <mplacek@concisesoftware.com>
2017-02-23 05:43:41 +0100

    Fixed updates list

--- web-server/app/assets/javascripts/components/updates/list-of-updates.jsx
+++ web-server/app/assets/javascripts/components/updates/list-of-updates.jsx
@@ -11,39 +11,56 @@ define(function(require) {
     },
     componentWillUnmount: function(){
       this.props.Updates.removeWatch("poll-updates");
+      this.props.Packages.removeWatch("poll-updates-packages");
     },
     componentWillMount: function(){
       SotaDispatcher.dispatch({actionType: 'get-updates'});
+      SotaDispatcher.dispatch({actionType: 'search-packages-by-regex', regex: "."});
       this.props.Updates.addWatch("poll-updates", _.bind(this.forceUpdate, this, null));
+      this.props.Packages.addWatch("poll-updates-packages", _.bind(this.forceUpdate, this, null));
     },
     render: function() {
-      var rows = _.map(this.props.Updates.deref(), function(update) {
-        var startend = update.periodOfValidity.split("/");
-        return (
-          <tr key={update.id}>
-            <td>
-              {update.packageId.name}
-            </td>
-            <td>
-              {update.packageId.version}
-            </td>
-            <td>
-              {startend[0]}
-            </td>
-            <td>
-              {startend[1]}
-            </td>
-            <td>
-              {update.priority}
-            </td>
-            <td>
-              <Router.Link to='update' params={{id: update.id}}>
-                Details
-              </Router.Link>
-            </td>
-          </tr>
-        );
-      });
+      var rows = [];
+      if(!_.isUndefined(this.props.Packages)) {
+        rows = _.map(this.props.Updates.deref(), function(update) {
+          var foundPackage = _.findWhere(this.props.Packages.deref(), {uuid: update.packageUuid});
+          var startend = update.periodOfValidity.split("/");
+          return (
+            <tr key={update.id}>
+              <td>
+                {!_.isUndefined(foundPackage) ? 
+                  foundPackage.id.name
+                :
+                  null
+                }
+              </td>
+              <td>
+                {!_.isUndefined(foundPackage) ? 
+                  foundPackage.id.version
+                :
+                  null
+                }
+              </td>
+              <td>
+                {startend[0]}
+              </td>
+              <td>
+                {startend[1]}
+              </td>
+              <td>
+                {update.priority}
+              </td>
+              <td>
+                <Router.Link to='update' params={{id: update.id}}>
+                  Details
+                </Router.Link>
+              </td>
+            </tr>
+          );
+        }, this);
+      }
+     
+      
       return (
         <div>
           <div className="row">
