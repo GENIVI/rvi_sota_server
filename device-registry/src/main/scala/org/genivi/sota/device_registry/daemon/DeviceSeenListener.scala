b9513fcaf9ca5162be0b3b81041fb40798f43f4e
Jochen Schneider <jochen@advancedtelematic.com>
2017-04-25 16:29:18 +0200

    Remove NotSeen status if device has been seen

--- device-registry/src/main/scala/org/genivi/sota/device_registry/daemon/DeviceSeenListener.scala
+++ device-registry/src/main/scala/org/genivi/sota/device_registry/daemon/DeviceSeenListener.scala
@@ -1,6 +1,7 @@
 package org.genivi.sota.device_registry.daemon
 
 import akka.Done
+import org.genivi.sota.data.DeviceStatus
 import org.genivi.sota.device_registry.db.DeviceRepository
 import org.genivi.sota.messaging.MessageBusPublisher
 import org.genivi.sota.messaging.Messages.{DeviceActivated, DeviceSeen}
@@ -17,10 +18,13 @@ object DeviceSeenListener {
             (implicit db: Database, ec: ExecutionContext): Future[Done] = {
 
           db.run(DeviceRepository.updateLastSeen(msg.uuid, msg.lastSeen)).flatMap { case (activated, ns) =>
-            if(activated)
-              messageBus.publishSafe(DeviceActivated(ns, msg.uuid, msg.lastSeen))
-            else
+            if (activated) {
+              messageBus.publishSafe(DeviceActivated(ns, msg.uuid, msg.lastSeen)).flatMap {_ =>
+                db.run(DeviceRepository.setDeviceStatus(msg.uuid, DeviceStatus.UpToDate))
+              }
+            } else {
               Future.successful(Done)
+            }
           }.recover {
             case ex =>
               _logger.warn(s"Could not process $msg", ex)
