e5e47d13bba5f8d4b8772341d16e18ee13de2135
Simão Mata <simao@advancedtelematic.com>
2017-06-06 16:48:05 +0200

    Revert "Simplify MessageBusListenerActor"
    This reverts commit 357c57f252ed4f6110803a1b2bcc775db84c91f0.

--- common-messaging/src/main/scala/org/genivi/sota/messaging/daemon/MessageBusListenerActor.scala
+++ common-messaging/src/main/scala/org/genivi/sota/messaging/daemon/MessageBusListenerActor.scala
@@ -18,9 +18,21 @@ class MessageBusListenerActor[M](source: Source[M, NotUsed])(implicit messageLik
   implicit val materializer = ActorMaterializer()
   implicit val ec = context.dispatcher
 
-  override def postRestart(reason: Throwable): Unit = {
-    log.error(reason, "Listener died, subscribing again")
-    trySubscribeDelayed()
+  override def postRestart(reason: Throwable): Unit = trySubscribeDelayed()
+
+  private def subscribed: Receive = {
+    log.info(s"Subscribed to ${messageLike.streamName}")
+
+    {
+      case Failure(ex) =>
+        log.error(ex, "Source/Listener died, subscribing again")
+        trySubscribeDelayed()
+        context become idle
+      case Done =>
+        log.info("Source finished, subscribing again")
+        trySubscribeDelayed()
+        context become idle
+    }
   }
 
   private def subscribe(): Unit = {
@@ -32,24 +44,18 @@ class MessageBusListenerActor[M](source: Source[M, NotUsed])(implicit messageLik
 
     source.runWith(sink).pipeTo(self)
 
-    log.info(s"Subscribed to ${messageLike.streamName}")
+    context become subscribed
   }
 
-  override def receive: Receive = {
-    case Failure(ex) =>
-      log.error(ex, "Source/Listener died, subscribing again")
-      trySubscribeDelayed()
-
-    case Done =>
-      log.info("Source finished, subscribing again")
-      trySubscribeDelayed()
-
+  private def idle: Receive = {
     case Subscribe =>
       Try(subscribe()).failed.foreach { ex =>
         log.error(ex, "Could not subscribe, trying again")
         trySubscribeDelayed()
       }
   }
+
+  override def receive: Receive = idle
 
   private def trySubscribeDelayed(delay: FiniteDuration = 5.seconds): Unit = {
     context.system.scheduler.scheduleOnce(delay, self, Subscribe)
